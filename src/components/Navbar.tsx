6e8f89625f5edd4afd420b5af3c976388b3d84ba
iamdev-james <jamesoluwabukola87@gmail.com>
2024-12-13 21:17:34 +0100

    Made some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,76 +1,86 @@
-import Link from 'next/link'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import { buttonVariants } from './ui/button'
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
 import {
   LoginLink,
   RegisterLink,
   getKindeServerSession,
-} from '@kinde-oss/kinde-auth-nextjs/server'
-import { ArrowRight } from 'lucide-react'
-import UserAccountNav from './UserAccountNav'
-import MobileNav from './MobileNav'
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
+import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
+import Image from 'next/image';
 
 const Navbar = () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
   return (
-    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-          <Link
-            href='/'
-            className='flex z-40 font-semibold'>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          {/* <Image
+            src="/thumbnail.png"
+            alt="platform logo"
+            width={364}
+            height={66}
+            quality={100}
+            className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+          /> */}
+          <Link href="/" className="flex z-40 font-semibold">
             <span>docmate.</span>
           </Link>
 
           <MobileNav isAuth={!!user} />
 
-          <div className='hidden items-center space-x-4 sm:flex'>
+          <div className="hidden items-center space-x-4 sm:flex">
             {!user ? (
               <>
                 <Link
-                  href='/pricing'
+                  href="/pricing"
                   className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
                   Pricing
                 </Link>
                 <LoginLink
                   className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
                   Sign in
                 </LoginLink>
                 <RegisterLink
                   className={buttonVariants({
-                    size: 'sm',
-                  })}>
-                  Get started{' '}
-                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                    size: "sm",
+                  })}
+                >
+                  Get started <ArrowRight className="ml-1.5 h-5 w-5" />
                 </RegisterLink>
               </>
             ) : (
               <>
                 <Link
-                  href='/dashboard'
+                  href="/dashboard"
                   className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
                   Dashboard
                 </Link>
 
                 <UserAccountNav
                   name={
                     !user.given_name || !user.family_name
-                      ? 'Your Account'
+                      ? "Your Account"
                       : `${user.given_name} ${user.family_name}`
                   }
-                  email={user.email ?? ''}
-                  imageUrl={user.picture ?? ''}
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ""}
                 />
               </>
             )}
@@ -78,7 +88,7 @@ const Navbar = () => {
         </div>
       </MaxWidthWrapper>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
