c37eb41ec902a26c2f8e19c8ee6cec6d6e8b4f6c
iamdev-james <jamesoluwabukola87@gmail.com>
2024-12-20 07:40:20 +0100

    Added footer to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
-import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  ArrowRight,
+  Facebook,
+  Github,
+  Send,
+  Instagram,
+  Figma,
+  Youtube,
+} from "lucide-react";
+
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -159,23 +169,87 @@ export default function Home() {
       </div>
 
       {/* Footer section */}
-      <MaxWidthWrapper className="mx-auto mb-32 mt-32 sm:mt-56">
-        <div className="flex space-between items-center">
-          <div>
-            <p className="text-bold">DOCMATE.</p>
-          </div>
+      <MaxWidthWrapper className="mx-auto mb-32 mt-12 sm:mt-56">
+        <div className="flex justify-between items-center">
+          <p className="font-bold">DOCMATE.</p>
           <div>
             <Link
               className={buttonVariants({
                 size: "lg",
                 variant: "outline",
-                className: "mt-5",
+                className: "",
               })}
               href="/dashboard"
               target="_blank"
             >
               Contact Us <ArrowRight className="ml-2 h-5 w-5" />
             </Link>
+          </div>
+        </div>
+        <hr className="my-10" />
+        <div className="flex justify-between items-start">
+          <div className="flex items-start space-x-24 text-sm">
+            <div>
+              <p className="font-bold mb-7 text-lg">Product</p>
+              <div className="flex flex-col items-start space-y-4">
+                <Link href="/">Landing Page</Link>
+                <Link href="/">Popup Builder</Link>
+                <Link href="/">Web-design</Link>
+                <Link href="/">Content</Link>
+                <Link href="/">Integrations</Link>
+              </div>
+            </div>
+            <div>
+              <p className="font-bold mb-7 text-lg">Use Cases</p>
+              <div className="flex flex-col items-start space-y-4">
+                <Link href="/">Web-designers</Link>
+                <Link href="/">Marketers</Link>
+                <Link href="/">Small Business</Link>
+                <Link href="/">Website Builder</Link>
+              </div>
+            </div>
+            <div>
+              <p className="font-bold mb-7 text-lg">Resources</p>
+              <div className="flex flex-col items-start space-y-4">
+                <Link href="/">Academy</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Themes</Link>
+                <Link href="/">Hosting</Link>
+                <Link href="/">Developers</Link>
+                <Link href="/">Support</Link>
+              </div>
+            </div>
+            <div>
+              <p className="font-bold mb-7 text-lg">Company</p>
+              <div className="flex flex-col items-start space-y-4">
+                <Link href="/">About Us</Link>
+                <Link href="/">Careers</Link>
+                <Link href="/">FAQs</Link>
+                <Link href="/">Teams</Link>
+                <Link href="/">Contact Us</Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="font-bold mb-7">Follow us</p>
+            <div className="flex space-x-3">
+              <Facebook />
+              <Youtube />
+              <Github />
+              <Send />
+              <Instagram />
+              <Figma />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between items-center mt-16">
+          <p className="text-sm">&#169;2024 All Rights Reserved</p>
+          <div className="flex space-x-6 text-sm">
+            <Link href="/">Privacy Policy</Link>
+            <Link href="/">Terms of Use</Link>
+            <Link href="/">Sales and Refunds</Link>
+            <Link href="/">Legal</Link>
+            <Link href="/">Site Map</Link>
           </div>
         </div>
       </MaxWidthWrapper>
