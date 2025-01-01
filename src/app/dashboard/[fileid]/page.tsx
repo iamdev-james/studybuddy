85815a50f76dea1f00e2fb0808179eeb9cdbb73b
iamdev-james <jamesoluwabukola87@gmail.com>
2025-01-01 21:17:52 +0100

    Setting up file id

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,7 @@
+import ChatWrapper from '@/components/chat/ChatWrapper'
+import PdfRenderer from '@/components/PdfRenderer'
+import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { notFound, redirect } from 'next/navigation'
+
