3a0865b5f35aacbd78b4fe4477037854494df51d
iamdev-james <jamesoluwabukola87@gmail.com>
2025-01-05 19:15:33 +0100

    Hot refix

--- src/types/message.ts
+++ src/types/message.ts
@@ -7,4 +7,8 @@ type Messages = RouterOutput['getFileMessages']['messages']
 
 type OmitText = Omit<Messages[number], 'text'>
 
+type ExtendedText = {
+  text: string | JSX.Element
+}
+
 export type ExtendedMessage = OmitText & ExtendedText
