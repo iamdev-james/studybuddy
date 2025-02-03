b27bf578350e8c239f86af560df7a03761d42c81
iamdev-james <jamesoluwabukola87@gmail.com>
2025-02-03 23:19:28 +0100

    Hot fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -44,7 +44,6 @@ const Dashboard = ({ subscriptionPlan }: PageProps) => {
         <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
       </div>
 
-      {/* display all user files */}
       {files && files?.length !== 0 ? (
         <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
           {/* Files Logics */}
